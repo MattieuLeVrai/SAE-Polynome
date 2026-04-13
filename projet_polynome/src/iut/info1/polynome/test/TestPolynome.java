2f90c7f7747ec05b4b19b240527a42bd9a4cc0f8
mattieu.liao <mattieu.liao@A203D2.iut.local>
2026-04-13 10:54:40 +0200

    Methode test

--- projet_polynome/src/iut/info1/polynome/test/TestPolynome.java
+++ projet_polynome/src/iut/info1/polynome/test/TestPolynome.java
@@ -2,6 +2,7 @@ package iut.info1.polynome.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,8 +12,52 @@ class TestPolynome {
 	void setUp() throws Exception {
 	}
 
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
 	@Test
-	void test() {
+	final void testPolynome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testPolynomeDoubleArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testGetDegre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testGetCoefficient() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testGetLimitesMoinsInfini() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testGetLimitesPlusInfini() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testGetRacines() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testEstNul() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testToString() {
 		fail("Not yet implemented");
 	}
 
