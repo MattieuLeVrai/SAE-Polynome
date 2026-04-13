ad41676b117023632f462357ec34ff4003d2f6e0
mattieu.liao <mattieu.liao@A203D2.iut.local>
2026-04-13 10:47:26 +0200

    Création de la class de test JUnit

---
+++ projet_polynome/src/iut/info1/polynome/test/TestPolynome.java
@@ -0,0 +1,19 @@
+package iut.info1.polynome.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestPolynome {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
