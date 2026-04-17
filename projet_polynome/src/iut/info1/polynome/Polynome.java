9f998a172e1bdcdb985bc39c1663fc917e0f918c
Yanis LAURENCONT <yanis.laurencont@iut-rodez.fr>
2026-04-17 08:56:53 +0200

    Modification et correction de toute la classe Polynome

--- projet_polynome/src/iut/info1/polynome/Polynome.java
+++ projet_polynome/src/iut/info1/polynome/Polynome.java
@@ -6,7 +6,15 @@
 package iut.info1.polynome;
 
 /**
- * TODO Décrire ce que fait la class
+ * Représente un polynôme à coefficients réels et fournit des outils
+ * pour manipuler ses propriétés algébriques.
+ * Cette classe permet notamment de :
+ * <ul>
+ * 		<li>Déterminer le degré du polynôme.</li>
+ * 		<li>Calculer les limites aux voisinages de l'infini.</li>
+ * 		<li>Obtenir les coefficients pour une puissance donnée.</li>
+ * 		<li>Générer une représentation textuelle formatée de l'expression.</li>
+ * </ul>
  * @author Higounet Kelvin
  * @author Laurençont Yanis
  * @author Liao Mattieu
@@ -32,12 +40,38 @@ public class Polynome {
      * @param coefficients Le tableau des coefficients donnés par l'utilisateur
      */
     public Polynome(double[] coefficients) {
-        // TODO: Copier le tableau reçu dans l'attribut de la classe
     	if (coefficients == null || coefficients.length == 0) {
-    		this.coefficients = new double[] {0.0};
-    	} else {
-    		this.coefficients = coefficients;
-    	}
+            throw new IllegalArgumentException("Le tableau ne doit pas être null ou vide.");
+        }
+        
+        this.coefficients = coefficients;
+
+        // Calcul des racines pour les degrés 1 et 2
+        int degre = this.getDegre();
+
+        if (degre == 2) {
+            double coeffA = getCoefficient(2);
+            double coeffB = getCoefficient(1);
+            double coeffC = getCoefficient(0);
+
+            // Le discriminant détermine le nombre de racines réelles
+            double discriminant = (coeffB * coeffB) - (4 * coeffA * coeffC);
+
+            if (discriminant > 0) {
+                this.racines = new double[] {
+                    (-coeffB - Math.sqrt(discriminant)) / (2 * coeffA),
+                    (-coeffB + Math.sqrt(discriminant)) / (2 * coeffA)
+                };
+            } else if (discriminant == 0) {
+                this.racines = new double[] { -coeffB / (2 * coeffA) };
+            } else {
+                this.racines = new double[0];
+            }
+        } else if (degre == 1) {
+            this.racines = new double[] { -getCoefficient(0) / getCoefficient(1) };
+        } else {
+            this.racines = new double[0];
+        }
     }
     
     /**
@@ -45,16 +79,12 @@ public class Polynome {
      * @return Le degré (entier)
      */
     public int getDegre() {
-        // TODO: Écrire la logique pour trouver et retourner le degré
-    	// On part de la fin du tableau (le plus haut degré potentiel)
-    	for (int indiceCoefficient = 0; indiceCoefficient <= this.coefficients.length - 1; indiceCoefficient++) {
-            if (this.coefficients[indiceCoefficient] != 0) {
-                return indiceCoefficient; // On a trouvé le terme de plus haut degré
+    	// On cherche l'indice du premier coefficient non nul en partant du début
+    	for (int i = 0; i < this.coefficients.length; i++) {
+            if (this.coefficients[i] != 0) {
+                return this.coefficients.length - 1 - i;
             }
         }
-        
-        // Si tous les coefficients sont nuls, le degré est techniquement 0 
-        // (ou parfois défini comme -1 ou -infini pour le polynôme nul)
         return 0;
     }
     
@@ -64,13 +94,13 @@ public class Polynome {
      * @return Le coefficient (réel)
      */
     public double getCoefficient(int puissance) {
-        // Retourner la valeur située au bon index du tableau
-    	int degreMin = this.coefficients.length - 1;
+        // Retourner la valeur située au bon index du tableau (inversion d'indice)
+    	int indice = this.coefficients.length - 1 - puissance;
     	
-    	if (puissance < 0 || puissance >= this.coefficients.length) {
-            return 0.0; // Un polynôme a un coefficient de 0 pour les puissances hors de son degré
+    	if (indice < 0 || indice >= this.coefficients.length) {
+            return 0.0;
         }
-    	return this.coefficients[degreMin - puissance];
+    	return this.coefficients[indice];
     }
     
     /**
@@ -78,8 +108,16 @@ public class Polynome {
      * @return La limite (peut utiliser Double.NEGATIVE_INFINITY ou POSITIVE_INFINITY)
      */
     public double getLimitesMoinsInfini() {
-        // TODO: Calculer la limite en fonction du monôme de plus haut degré
-        return 0.0;
+    	int n = this.getDegre();
+        double an = this.getCoefficient(n);
+
+        if (n == 0) return an;
+
+        if (n % 2 == 0) {
+            return (an > 0) ? Double.POSITIVE_INFINITY : Double.NEGATIVE_INFINITY;
+        } else {
+            return (an > 0) ? Double.NEGATIVE_INFINITY : Double.POSITIVE_INFINITY;
+        }
     }
     
     /**
@@ -87,8 +125,12 @@ public class Polynome {
      * @return La limite
      */
     public double getLimitesPlusInfini() {
-        // TODO: Calculer la limite en fonction du monôme de plus haut degré
-        return 0.0;
+    	int n = this.getDegre();
+        double an = this.getCoefficient(n);
+
+        if (n == 0) return an;
+
+        return (an > 0) ? Double.POSITIVE_INFINITY : Double.NEGATIVE_INFINITY;
     }
     
     /**
@@ -105,9 +147,8 @@ public class Polynome {
      * @return Vrai si c'est le polynôme nul, Faux sinon
      */
     public boolean estNul() {
-        // TODO: Parcourir le tableau pour vérifier si tous les coefficients valent 0
-        for (int indiceCoefficient = 0; indiceCoefficient <= this.coefficients.length - 1; indiceCoefficient++) {
-        	if (this.coefficients[indiceCoefficient] != 0) {
+        for (double coeff : this.coefficients) {
+        	if (coeff != 0) {
         		return false;
             }
         }
@@ -120,8 +161,36 @@ public class Polynome {
      */
     @Override
     public String toString() {
-        // TODO: Construire la chaîne d'affichage
-        return "";
+    	if (this.estNul()) return "0.0";
+
+        StringBuilder sb = new StringBuilder();
+        boolean premierTerme = true;
+
+        for (int i = 0; i < this.coefficients.length; i++) {
+            double coeff = this.coefficients[i];
+            if (coeff == 0) continue;
+
+            int puissance = this.coefficients.length - 1 - i;
+
+            if (coeff > 0 && !premierTerme) {
+                sb.append(" + ");
+            } else if (coeff < 0) {
+                sb.append(premierTerme ? "-" : " - ");
+            }
+
+            double absCoeff = Math.abs(coeff);
+            if (absCoeff != 1 || puissance == 0) {
+                sb.append(absCoeff);
+            }
+
+            if (puissance > 0) {
+                sb.append("X");
+                if (puissance > 1) {
+                    sb.append("^").append(puissance);
+                }
+            }
+            premierTerme = false;
+        }
+        return sb.toString();
     }
-	
 }
