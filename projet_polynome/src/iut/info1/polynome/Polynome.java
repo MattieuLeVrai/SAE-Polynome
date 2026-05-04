7dd4a03723c6669347f0eee892457d14af902e43
ouafa <ouafa@10.109.0.27>
2026-05-04 07:54:47 +0200

    mise a jour polynome test ok

--- projet_polynome/src/iut/info1/polynome/Polynome.java
+++ projet_polynome/src/iut/info1/polynome/Polynome.java
@@ -5,15 +5,18 @@
 
 package iut.info1.polynome;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Représente un polynôme à coefficients réels et fournit des outils
  * pour manipuler ses propriétés algébriques.
  * Cette classe permet notamment de :
  * <ul>
- * 		<li>Déterminer le degré du polynôme.</li>
- * 		<li>Calculer les limites aux voisinages de l'infini.</li>
- * 		<li>Obtenir les coefficients pour une puissance donnée.</li>
- * 		<li>Générer une représentation textuelle formatée de l'expression.</li>
+ * <li>Déterminer le degré du polynôme.</li>
+ * <li>Calculer les limites aux voisinages de l'infini.</li>
+ * <li>Obtenir les coefficients pour une puissance donnée.</li>
+ * <li>Générer une représentation textuelle formatée de l'expression.</li>
  * </ul>
  * @author Higounet Kelvin
  * @author Laurençont Yanis
@@ -23,7 +26,6 @@ package iut.info1.polynome;
 public class Polynome {
 
 	private double[] coefficients;
-    private double[] racines;
     
     /**
      * Constructeur par défaut.
@@ -32,11 +34,10 @@ public class Polynome {
     public Polynome() {
     	// Initialisation des tableaux pour le polynôme nul
     	this.coefficients = new double[] {0.0};
-        this.racines = new double[0];
     }
     
     /**
-     * Constructeur à partir d'un tableau de coefficients.
+     * Constructeur à partir d'un tableau de coefficients
      * @param coefficients Le tableau des coefficients donnés par l'utilisateur
      */
     public Polynome(double[] coefficients) {
@@ -45,33 +46,6 @@ public class Polynome {
         }
         
         this.coefficients = coefficients;
-
-        // Calcul des racines pour les degrés 1 et 2
-        int degre = this.getDegre();
-
-        if (degre == 2) {
-            double coeffA = getCoefficient(2);
-            double coeffB = getCoefficient(1);
-            double coeffC = getCoefficient(0);
-
-            // Le discriminant détermine le nombre de racines réelles
-            double discriminant = (coeffB * coeffB) - (4 * coeffA * coeffC);
-
-            if (discriminant > 0) {
-                this.racines = new double[] {
-                    (-coeffB - Math.sqrt(discriminant)) / (2 * coeffA),
-                    (-coeffB + Math.sqrt(discriminant)) / (2 * coeffA)
-                };
-            } else if (discriminant == 0) {
-                this.racines = new double[] { -coeffB / (2 * coeffA) };
-            } else {
-                this.racines = new double[0];
-            }
-        } else if (degre == 1) {
-            this.racines = new double[] { -getCoefficient(0) / getCoefficient(1) };
-        } else {
-            this.racines = new double[0];
-        }
     }
     
     /**
@@ -79,13 +53,13 @@ public class Polynome {
      * @return Le degré (entier)
      */
     public int getDegre() {
-    	// On cherche l'indice du premier coefficient non nul en partant du début
-    	for (int i = 0; i < this.coefficients.length; i++) {
+    	// On cherche l'indice du premier coefficient non nul en partant de la fin
+    	for (int i = this.coefficients.length - 1; i >= 0; i--) {
             if (this.coefficients[i] != 0) {
-                return this.coefficients.length - 1 - i;
+                return i;
             }
         }
-        return 0;
+        return 0; // Polynome nul
     }
     
     /**
@@ -94,13 +68,10 @@ public class Polynome {
      * @return Le coefficient (réel)
      */
     public double getCoefficient(int puissance) {
-        // Retourner la valeur située au bon index du tableau (inversion d'indice)
-    	int indice = this.coefficients.length - 1 - puissance;
-    	
-    	if (indice < 0 || indice >= this.coefficients.length) {
+    	if (puissance < 0 || puissance >= this.coefficients.length) {
             return 0.0;
         }
-    	return this.coefficients[indice];
+    	return this.coefficients[puissance];
     }
     
     /**
@@ -138,8 +109,78 @@ public class Polynome {
      * @return Un tableau de réels
      */
     public double[] getRacines() {
-        // Retourner l'attribut racines
-        return this.racines;
+    	if (this.estNul() || this.getDegre() == 0) {
+            return new double[0]; // Pas de racine (ou infinité si P=0, on retourne vide)
+        }
+
+        // 1. Calcul automatique de l'intervalle de recherche
+        int n = this.getDegre();
+        double an = Math.abs(this.getCoefficient(n));
+        double maxCoeff = 0;
+        
+        for (int i = 0; i < n; i++) {
+            maxCoeff = Math.max(maxCoeff, Math.abs(this.getCoefficient(i)));
+        }
+        
+        // Toutes les racines se trouvent forcément entre -limite et +limite
+        double limite = 1.0 + (maxCoeff / an); 
+        
+        // 2. Recherche par balayage et dichotomie
+        List<Double> listeRacines = new ArrayList<>();
+        
+        // Si la limite est immense, on adapte un peu le pas
+        // Sinon on garde un pas classique de 0.1
+        double pas = (limite > 1000) ? (limite / 10000.0) : 0.1; 
+
+        for (double x = -limite; x < limite; x += pas) {
+            double y1 = evaluer(x);
+            double y2 = evaluer(x + pas);
+
+            // Si la courbe traverse l'axe (changement de signe)
+            if (y1 * y2 <= 0) {
+                double a = x;
+                double b = x + pas;
+                double precision = 1e-6; // Précision à 6 décimales
+
+                // Dichotomie pour affiner la position de la racine
+                while ((b - a) > precision) {
+                    double m = (a + b) / 2.0;
+                    if (evaluer(a) * evaluer(m) <= 0) {
+                        b = m; 
+                    } else {
+                        a = m; 
+                    }
+                }
+
+                // Arrondi à 4 décimales pour gérer les impécisions
+                double racine = Math.round(((a + b) / 2.0) * 10000.0) / 10000.0;
+                
+                if (!listeRacines.contains(racine)) {
+                    listeRacines.add(racine);
+                }
+            }
+        }
+
+        // 3. Conversion de la liste en tableau de double
+        double[] tableau = new double[listeRacines.size()];
+        for (int i = 0; i < listeRacines.size(); i++) {
+            tableau[i] = listeRacines.get(i);
+        }
+        return tableau;
+    }
+    
+    /**
+     * Calcule la valeur de P(x) pour un x donné (indispensable pour la dichotomie).
+     * @param x La valeur à tester
+     * @return Le résultat de l'équation
+     */
+    public double evaluer(double x) {
+        double resultat = 0;
+        // On utilise directement 'i' comme puissance
+        for (int i = 0; i < this.coefficients.length; i++) {
+            resultat += this.coefficients[i] * Math.pow(x, i); 
+        }
+        return resultat;
     }
     
     /**
@@ -166,11 +207,10 @@ public class Polynome {
         StringBuilder sb = new StringBuilder();
         boolean premierTerme = true;
 
-        for (int i = 0; i < this.coefficients.length; i++) {
-            double coeff = this.coefficients[i];
+        // On parcourt à l'envers pour afficher d'abord les X de plus haut degré
+        for (int puissance = this.coefficients.length - 1; puissance >= 0; puissance--) {
+            double coeff = this.coefficients[puissance];
             if (coeff == 0) continue;
-
-            int puissance = this.coefficients.length - 1 - i;
 
             if (coeff > 0 && !premierTerme) {
                 sb.append(" + ");
